d5afa6aa1b40a29db818b156139c50d683407e1b
heatblazer <ilianzapryanov@abv.bg>
2017-01-10 13:03:10 +0200

    destructor test and out of scope things that happen

---
+++ dtor.py
@@ -0,0 +1,27 @@
+"""experimenting with out of scope objects and destrution"""
+
+class A():
+
+    def __init__(self):
+        print("A()")
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("exit A")
+
+    def __del__(self):
+        print("~A()")
+
+
+def main():
+
+    if True:
+        a = A()
+
+    print("Exiting main...")
+
+if __name__ == "__main__":
+    main()
+
