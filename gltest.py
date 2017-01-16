313dd64a7022f83f32731488f6ce5295b779ef25
heatblazer <ilianzapryanov@abv.bg>
2017-01-16 15:35:23 +0200

    pyglet and pywavefront added

--- gltest.py
+++ gltest.py
@@ -7,38 +7,75 @@ from PyQt5.Qt import QVector3D, QVector2D
 from PyQt5.Qt import QTextStream
 from PyQt5.Qt import QImage
 from PyQt5.Qt import QFile, QIODevice, QFileInfo
-
-# helcper class for facing represenitng the stuf from .obj file
-class Face():
-    def __init__(self):
-        self.v = QVector3D()
-        self.vn = QVector3D()
-        self.f = QVector2D()
+from OpenGL.GL import *
+from OpenGL import *
 
 
-class Model():
-    def __init__(self, fname):
-        self.fname = fname
-        self.textureName = None
-        self.Vertices = list()
-        self.VNormals = list()
-        self.UVs = list()
-        self.faces = list()
+def MTL(filename):
+    contents = {}
+    mtl = None
+    for line in open(filename, "r"):
+        if (line.startswith('#')): continue
+        values = line.split()
+        if not values: continue
+        if values[0] == "newmtl":
+            mtl = contents[values[1]] = {}
+        elif mtl is None:
+            raise ValueError("mtl does not start with newmtl stmt")
+        elif values[0] == "map_Kd":
+            mtl[values[0]] = values[1]
+            surface = QImage()
+            surface.load()
 
-    def render(self):
-        pass
 
-    def face(self):
-        pass
 
-    def points(self):
-        pass
 
-    def __LoadMTL(self, fn, mtlName):
-        pass
+class OBJ():
+    def __init__(self, filename, swapyz=False):
+        self.vertices = []
+        self.normals = []
+        self.textcoords = []
+        self.faces = []
 
-    def __LoadTexture(self):
-        pass
+        material = None
+        for line in open(filename, "r"):
+            if line.startswith('#'):
+                continue
+            values = line.split()
+            if not values:
+                continue
+            if values[0] == "v":
+                v = map(float, values[1:4])
+                if swapyz:
+                    v = v[0], v[2], v[1]
+                self.vertices.append(v)
+            elif values[0] == "vn":
+                v = map(float, values[1:4])
+                if swapyz:
+                    v = v[0], v[2], v[1]
+                self.normals.append(v)
+            elif values[0] == "vt":
+                self.textcoords.append(map(float, values[1:3]))
+            elif values[0] in ("usemtl", "usemat"):
+                material = values[1]
+            elif values[0] == "mtllib":
+                self.mtl = MTL(values[1])
+            elif values[0] == "f":
+                face = []
+                texcoords = []
+                norms = []
+                for v in values[1:]:
+                    w = v.split("/")
+                    face.append(int(w[0]))
+                    if len(w) >= 2 and len(w[1]) > 0:
+                        texcoords.append(int(w[1]))
+                    else:
+                        texcoords.append(0)
+                    if len(w) >= 3 and len(w[2]) > 0:
+                        norms.append(int(w[2]))
+                    else:
+                        norms.append(0)
+                self.faces.append(face, norms, texcoords, material)
 
 
 class Widget(QWidget):
